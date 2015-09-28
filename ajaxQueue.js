67b8e61dc338ab2c73a2dfd97bd5b28936e6a0f5
Shawn Parker <shawn@topfroggraphics.com>
2015-09-28 06:37:31 -0700

    Minor cleanup. Nothing new.

--- ajaxQueue.js
+++ ajaxQueue.js
@@ -8,44 +8,14 @@
 	 */
 	ajaxQueue = function() {
 		/**
-		 * Qeueue object init
-		 */
-		var _aQueue = $({});
-		/**
-		 * Add to queue
-		 *
-		 * @param string url - ajax request uri
-		 * @param string type - type of ajax request, 'post' or 'get'
-		 * @param object opts - standard jQuery Ajax request opts. See: http://api.jquery.com/jQuery.ajax/
-		 */
-		this.add = function(url, type, options) {
-			_aQueue.queue('requests', function() {
-				var request = new aRequest(url, type, options);
-				request.call();
-			});
-		};
-		/**
-		 * run queue
-		 */
-		this.run = function() {
-			_aQueue.dequeue('requests');
-		};
-		/**
-		 * flush queue
-		 */
-		this.flush = function() {
-			_aQueue.queue('requests', []);
-		};
-
-		/**
 		 * Request object
 		 * Handles the abstraction of ajax 'complete' method for running in the queue
 		 * @see aQueue.add for param usage
 		 */
-		var aRequest = function(url, type, opts) {
+		var aRequest = function(url, method, opts) {
 			this.opts = opts || {};
 			this.opts.url = url;
-			this.opts.type = type || 'get';
+			this.opts.method = method || 'get';
 
 			var _complete = this.opts.complete || function() {};
 			this.opts.complete = function(r) {
@@ -53,11 +23,50 @@
 				_aQueue.dequeue('requests');
 			};
 		};
+
 		/**
 		 * aRequest.call - perform ajax request
 		 */
 		aRequest.prototype.call = function() {
 			$.ajax(this.opts);
+		};
+
+		/**
+		 * Qeueue object init
+		 */
+		var _aQueue = $({});
+
+		/**
+		 * Add an empty .complete() function on the queue object
+		 */
+		_aQueue.complete = function(){};
+
+		/**
+		 * Add to queue
+		 *
+		 * @param string url - ajax request uri
+		 * @param string method - http method of ajax request, 'post' or 'get'
+		 * @param object opts - standard jQuery Ajax request opts. See: http://api.jquery.com/jQuery.ajax/
+		 */
+		this.add = function(url, method, options) {
+			_aQueue.queue('requests', function() {
+				var request = new aRequest(url, method, options);
+				request.call();
+			});
+		};
+
+		/**
+		 * run queue
+		 */
+		this.run = function() {
+			_aQueue.dequeue('requests');
+		};
+
+		/**
+		 * flush queue
+		 */
+		this.flush = function() {
+			_aQueue.queue('requests', []);
 		};
 	};	
 })(jQuery);
